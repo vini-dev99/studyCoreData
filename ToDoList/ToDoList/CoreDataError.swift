e67c59dd56f6669ef1ced0d6b82d051840d47f0d
Vinicius Flores Ribeiro <viniciusfloresribeiro@gmail.com>
2022-04-28 15:07:08 -0300

    new archives

---
+++ ToDoList/ToDoList/CoreDataError.swift
@@ -0,0 +1,12 @@
+//
+//  CoreDataError.swift
+//  ToDoList
+//
+//  Created by Vinícius Flores Ribeiro on 28/04/22.
+//
+
+import Foundation
+
+struct CoreDataError {
+    static let noAppDelegate = NSError(domain: "No App Delegate", code: 1, userInfo: nil)
+}
--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import SparkUI
+import Layoutless
 
-class ViewController: UIViewController {
+class ViewController: SViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+    var todos: [String] = []
+//    lazy var addBarButtonIem = UIBarButtonItem(title: <#T##String?#>, action: <#T##(T) -> Void#>)
 
 
 }
